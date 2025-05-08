8c01030bc00125c9545467d1558b1d805bec1a31
TimothyAK <mothyabraham973@gmail.com>
2025-05-09 04:52:49 +0700

    Fixec budget check bug

--- expensesService/routers/expenseRouter.js
+++ expensesService/routers/expenseRouter.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.get("/user/:userID", expenseController.getExpenseByUserId)
 router.get("/:expenseID", expenseController.getExpenseById)
 router.post("", expenseController.createExpense);
-router.post("/mny/:page", expenseController.getExpensePerMnYByUserId)
 router.post("/mny/sum", expenseController.getSumByMonth)
 router.post("/mny/gbC", expenseController.getExpenseGroupByCategory)
+router.post("/mny/:page", expenseController.getExpensePerMnYByUserId)
 router.put("/:expenseID", expenseController.updateExpense)
 router.delete("/:expenseID", expenseController.deleteExpense)
 
