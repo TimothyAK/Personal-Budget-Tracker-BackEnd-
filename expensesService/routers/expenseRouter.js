7ea6a60769b9bc5e0a3da22eda5b956e250528f3
TimothyAK <mothyabraham973@gmail.com>
2025-05-09 02:39:25 +0700

    Modified routing

--- expensesService/routers/expenseRouter.js
+++ expensesService/routers/expenseRouter.js
@@ -3,12 +3,12 @@ const expenseController = require('../controllers/expenseController');
 
 const router = express.Router();
 
-router.get("/:expenseID", expenseController.getExpenseById)
 router.get("/user/:userID", expenseController.getExpenseByUserId)
+router.get("/:expenseID", expenseController.getExpenseById)
 router.post("", expenseController.createExpense);
-router.post("/sum", expenseController.getSumByMonth)
-router.post("/mny", expenseController.getExpensePerMnYByUserId)
-router.post("/gbC", expenseController.getExpenseGroupByCategory)
+router.post("/mny/", expenseController.getExpensePerMnYByUserId)
+router.post("/mny/sum", expenseController.getSumByMonth)
+router.post("/mny/gbC", expenseController.getExpenseGroupByCategory)
 router.put("/:expenseID", expenseController.updateExpense)
 router.delete("/:expenseID", expenseController.deleteExpense)
 
